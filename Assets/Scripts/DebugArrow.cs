227271d9b92378d1fe617c0fd130bdbedfb24bf2
Alexander <gastec139@gmail.com>
2025-08-03 21:28:30 +0300

    add debug arrow component

---
+++ Assets/Scripts/DebugArrow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class DebugArrow : MonoBehaviour
+{
+    public static void ForGizmo(Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
+    {
+        Gizmos.color = color;
+        Gizmos.DrawRay(pos, direction);
+
+        Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Gizmos.DrawRay(pos + direction, right * arrowHeadLength);
+        Gizmos.DrawRay(pos + direction, left * arrowHeadLength);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        ForGizmo(gameObject.transform.position, (gameObject.transform.rotation * Vector3.forward)*0.3f, Color.red, 0.25f, 30);
+    }
+}
