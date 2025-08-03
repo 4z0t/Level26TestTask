5102fb177f03cb056492f701d7ea4c6c6916716d
Alexander <gastec139@gmail.com>
2025-08-03 23:36:59 +0300

    add input

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent (typeof(TurretRotator))]
+public class Input : MonoBehaviour
+{
+    [SerializeField] private TurretRotator _rotator;
+
+    [Header("Настройки")]
+    public float sensitivity = 0.5f; // Чувствительность
+
+    private float yaw;               // Горизонтальный угол
+    private float pitch;             // Вертикальный угол
+    private Vector2 lookInput;       // Входные данные от Input System
+
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+        if (lookInput.magnitude > 0.1f) // Игнорируем микро-движения
+        {
+            // Меняем углы
+            yaw += lookInput.x * sensitivity;
+            pitch -= lookInput.y * sensitivity; // "-" для инверсии вертикали
+
+            (pitch, yaw) = _rotator.SetRotation(pitch, yaw);
+        }
+    }
+
+    //float progress = 0f;
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    progress = (Time.deltaTime + progress) % 1;
+    //    _rotator.SetRotation(Mathf.Lerp(-45, 45, Mathf.Sin(progress * Mathf.PI)), Mathf.Lerp(0, 360, progress));
+    //}
+}
